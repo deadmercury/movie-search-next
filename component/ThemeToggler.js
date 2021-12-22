946a9c0e8fa18f79823b76bdd73c6649b5e9afd8
Kamran Ansari <crossthinking786@gmail.com>
2021-12-22 22:16:12 +0530

    add label to svg instead of button

--- component/ThemeToggler.js
+++ component/ThemeToggler.js
@@ -14,6 +14,7 @@ const ThemeToggler = () => {
       width="26"
       height="26"
       viewBox="0 0 24 24"
+      aria-label="Moon Icon - Switch to dark theme"
     >
       <path d="M12,11.807C9.349,9.155,8.7,5.261,10.049,2c-1.875,0.37-3.666,1.281-5.12,2.735c-3.905,3.905-3.905,10.237,0,14.142	c3.906,3.906,10.237,3.905,14.143,0c1.454-1.454,2.364-3.244,2.735-5.119C18.545,15.106,14.651,14.458,12,11.807z" />
     </svg>
@@ -26,6 +27,7 @@ const ThemeToggler = () => {
       height="26"
       viewBox="0 0 24 24"
       fill="white"
+      aria-label="Sun Icon - Switch to light theme"
     >
       <path d="M6.995 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007-2.246-5.007-5.007-5.007S6.995 9.239 6.995 12zM11 19H13V22H11zM11 2H13V5H11zM2 11H5V13H2zM19 11H22V13H19z" />
       <path
@@ -49,14 +51,7 @@ const ThemeToggler = () => {
   const svg = { light: moonSVG, dark: sunSVG };
 
   return (
-    <button
-      className={styles.button}
-      aria-labelledby="button-label"
-      onClick={changeTheme}
-    >
-      <span id="button-label" hidden>
-        Theme Toggler
-      </span>
+    <button className={styles.button} onClick={changeTheme}>
       {svg[theme]}
     </button>
   );
